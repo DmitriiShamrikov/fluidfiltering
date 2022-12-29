5e159b1b84b348cce2943b3f760504dc6c5a7121
blackoverlord <blackoverlordd@gmail.com>
2022-12-29 17:46:42 +0000

    basic UI with filters

--- control.lua
+++ control.lua
@@ -1,4 +1,9 @@
+g_SelectedEntity = nil
+g_Filters = {} -- should be in global
+
 function OpenFilterBox(event)
+	g_SelectedEntity = nil
+
 	if event.gui_type ~= defines.gui_type.entity or event.entity == nil then
 		return
 	end
@@ -12,24 +17,35 @@ function OpenFilterBox(event)
 		return
 	end
 
-	local name = 'liquid-filter-ui'
-	local filterFrame = nil
-	for key, child in pairs(player.gui.relative.children) do
-		if child.name == name then
-			filterFrame = child
-			break
-		end
-	end
-
+	local frameName = 'liquid-filter-ui'
+	local chooseButtonName = 'chooser'
+	local filterFrame = player.gui.relative[frameName]
 	if filterFrame == nil then
 		local anchor = {
 			gui = defines.relative_gui_type.entity_with_energy_source_gui,
 			position = defines.relative_gui_position.bottom,
 			name = 'filter-pump'
 		}
-		filterFrame = player.gui.relative.add{type='frame', name=name, anchor=anchor}
-		filterFrame.add{type='label', caption=player.name}
+		filterFrame = player.gui.relative.add{type='frame', name=frameName, caption='Filter', anchor=anchor}
+		filterFrame.add{type='choose-elem-button', name=chooseButtonName, elem_type='fluid'}
+	end
+
+	filterFrame[chooseButtonName].elem_value = g_Filters[event.entity.unit_number]
+	g_SelectedEntity = event.entity
+end
+
+function SetFilter(event)
+	if g_SelectedEntity == nil or event.element == nil then
+		return
+	end
+
+	g_Filters[g_SelectedEntity.unit_number] = event.element.elem_value
+
+	local player = game.get_player(event.player_index)
+	if player ~= nil then
+		player.print('Setting filter for the entity ' .. g_SelectedEntity.unit_number .. ': ' .. (event.element.elem_value or 'none'))
 	end
 end
 
 script.on_event(defines.events.on_gui_opened, OpenFilterBox)
+script.on_event(defines.events.on_gui_elem_changed, SetFilter)
